70d2693024a1cdb6f9aa0631b2738caa41278469
Frank Zhang <frankzhang123@gmail.com>
2022-10-13 21:10:19 +0800

    2

--- src/misc/msg.rs
+++ src/misc/msg.rs
@@ -6,7 +6,7 @@ use std::ops::Deref;
 use std::path::PathBuf;
 
 struct MessageMap {
-    lang_map: HashMap<&'static str, &'static str>,
+    lang_map: HashMap<String, String>,
 }
 
 impl MessageMap {
@@ -16,23 +16,30 @@ impl MessageMap {
         }
     }
 
-    pub fn add(&mut self, name: &'static str, value: &'static str) {
-        self.lang_map.insert(name, value);
+    pub fn set(&mut self, name: &str, value: String) {
+        self.lang_map.insert(String::from(name), String::from(value));
     }
 
-    pub fn get(&self, key: &'static str) -> &str {
+    pub fn get(&self, key: &str) -> &str {
         if let Some(value) = self.lang_map.get(key) {
             return value.deref()
         }
         key
     }
 
-    pub fn load_language_file(&mut self, file_path: PathBuf) -> Result<(), Box<dyn Error>> {
+    pub fn load_lang_file(&mut self, file_path: PathBuf) -> Result<(), Box<dyn Error>> {
         log::trace!("Parsing language file: {:#?}", file_path);
         let f = File::open(file_path)?;
         let reader = BufReader::new(f);
+        let mut name: &str = "";
+        let mut message: String = String::new();
 
-        for line in reader.lines() {
+        for line in reader.lines()? {
+            match line.get(0..1)? {
+                "%" => { /* Msg name*/ name = line[1..]; }
+                "." => { self.set(name, message.to_string()); message = String::new(); }
+                _ => { message.push_str(line.as_str()); message.push('\n') }
+            }
         }
         Ok(())
     }
@@ -41,7 +48,7 @@ impl MessageMap {
         log::trace!("Parsing language file: {:#?}", file_path);
         let reader = BufReader::new(File::open(file_path)?);
 
-        for line in reader.lines() {
+        for line in reader.lines()? {
         }
         Ok(())
     }
